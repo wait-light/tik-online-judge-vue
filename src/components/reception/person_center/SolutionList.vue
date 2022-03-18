46875279a2e23240b47474d4d4ff8b7cc3e30e9f
wait-light <waitlight99@gmail.com>
2022-03-18 19:12:10 +0800

    add personCenter solution manager

--- src/components/reception/person_center/SolutionList.vue
+++ src/components/reception/person_center/SolutionList.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
+    <div style="display:flex; align-items:center;justify-content:space-between">
         <h1>文章共{{ pageInfo.total }}篇</h1>
+        <el-button
+            v-if="isSelf"
+            type="success"
+            @click="router.push('/poster')"
+            size="mini"
+            style="height:10px"
+        >新增</el-button>
     </div>
     <!-- v-infinite-scroll="load" :infinite-scroll-disabled="!pageInfo.hasNext" -->
     <div class="post-list">
@@ -20,6 +27,14 @@
                     <span>{{ post.view }}</span>
                 </div>
             </div>
+            <div class="operate-box">
+                <el-button
+                    size="mini"
+                    type="primary"
+                    @click.stop="router.push(`/poster/${post.id}`)"
+                >修改</el-button>
+                <el-button size="mini" type="danger" @click.stop="deletePost(post.id)">删除</el-button>
+            </div>
         </div>
     </div>
     <el-pagination
@@ -36,10 +51,10 @@
 
 <script setup>
 import { View } from "@element-plus/icons";
-import { getData, getList } from "@/js/common_data_operation";
+import { deleteData, getData, getList } from "@/js/common_data_operation";
 import { computed, reactive, ref } from "@vue/reactivity";
 import { onMounted, watch } from "@vue/runtime-core";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { useStore } from "vuex";
 import { timeStr } from "@/js/DateUtil"
 const props = defineProps({
@@ -47,6 +62,7 @@ const props = defineProps({
 })
 const store = useStore();
 const route = useRoute()
+const router = useRouter()
 const pageInfo = ref({
     page: 1,
     pageSize: 3,
@@ -73,7 +89,12 @@ if (route.params.uid) {
     uid.value = store.state.user.user.uid
     watch(() => store.state.user.user.uid, uidChange)
 }
-
+const isSelf = computed(() => {
+    if (uid.value == store.state.user.user.uid) {
+        return true
+    }
+    return false
+})
 const pageSizeChange = (size) => {
     pageInfo.value.pageSize = size
     getSolutions()
@@ -97,6 +118,11 @@ const getSolutions = () => {
 const goPost = (id) => {
     window.open(`/post/${id}`, '_blank');
 }
+const deletePost = (id) => {
+    deleteData(`/social/post/${id}`, null, null, "确认删除该文章？").then(res => {
+        getSolutions()
+    })
+}
 onMounted(() => {
     getSolutions()
 })
@@ -105,14 +131,25 @@ onMounted(() => {
 <style lang="sass" scoped>
 
 @import "@/sass/_variables.sass"
+.operate-box
+    display: none
+    padding: 10px
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04)
+    position: absolute
+    right: 0
+    top: 50%
+    transform: translateY(-50%)
 .post-list
     background: #fff
     border-bottom: $slim-border
     .post-box
+        position: relative
         padding: 15px
         border-bottom: $slim-border
         &:hover
             cursor: pointer
+            .operate-box
+                display: block
         .post-header
             font-size: 14px
             display: flex
